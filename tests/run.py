664337bac0e07f2f0508b242cdf196e545c5ed42
tchar <tilemachos.charalampous@gmail.com>
2015-07-20 17:38:05 +0300

    Fixed some debug message logic.

--- tests/run.py
+++ tests/run.py
@@ -55,8 +55,8 @@ def parse_args():
     DEBUG = args.nodebug
 
 
-def debug(s):
-    if DEBUG:
+def debug(s, error=False):
+    if DEBUG or error:
         print(s)
 
 
@@ -81,205 +81,210 @@ alias2 = random_str()
 alias3 = random_str()
 
 
-def process_request(r_code, r_text):
+def process_request(d_msg, r_code, r_text):
     msg_code = 'Request returned code:{0}'.format(r_code)
     if r_text:
         msg_content = 'Response:{0}'.format(r_text)
     else:
         msg_content = 'Request did not return any content'
 
-    if DEBUG or r_code not in ACCEPTABLE_CODES:
-        print(msg_code)
-        print(msg_content)
+    if r_code not in ACCEPTABLE_CODES:
+        debug(d_msg, error=True)
+        debug(msg_code, error=True)
+        debug(msg_content, error=True)
+    else:
+        debug(d_msg)
+        debug(msg_code)
+        debug(msg_content)
 
 
 def test_register(pushbots):
-    debug('\nTesting register_token() with token:{0}, tags:[{1}], alias:{2}.'
-          .format(token1, tag1, alias1))
+    d_msg = ('\nTesting register_token() with token:{0}, tags:[{1}],'
+             ' alias:{2}.'.format(token1, tag1, alias1))
     c, d = register_token(pushbots=pushbots, token=token1, platform=platform,
                           tag=[tag1], lat='80.241', lng='13.241',
                           alias=alias1, active=['test1'])
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
-    debug('\nTesting register_token_simple() with token:{0}.'.format(token2))
+    d_msg = '\nTesting register_token_simple() with token:{0}.'.format(token2)
     c, d = register_token_simple(pushbots=pushbots, platform=platform,
                                  token=token2)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token3}
-    debug('\nTesting register_data() with custom data:{0}.'.format(data))
+    d_msg = '\nTesting register_data() with custom data:{0}.'.format(data)
     c, d = register_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_register_batch(pushbots):
-    debug('\nTesting register_batch_tokens() with tokens:{0} and tags:{1}.'
-          .format([token4, token5], [tag1, tag2]))
+    d_msg = ('\nTesting register_batch_tokens() with tokens:{0} and tags:{1}.'
+             .format([token4, token5], [tag1, tag2]))
     c, d = register_batch_tokens(pushbots=pushbots, platform=platform,
                                  tokens=[token4, token5], tags=[tag1, tag2])
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'tokens': [token6, token7],
             'tags': [tag1, tag2]}
-    debug('\nTesting register_batch_data() with tokens:{0} and tags:{1}.'
-          .format([token6, token7], [tag1, tag2]))
+    d_msg = ('\nTesting register_batch_data() with tokens:{0} and tags:{1}.'
+             .format([token6, token7], [tag1, tag2]))
     c, d = register_batch_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_alias(pushbots):
-    debug('\nTesting alias_token() with token:{0}, alias:{1}.'
-          .format(token2, alias2))
+    d_msg = ('\nTesting alias_token() with token:{0}, alias:{1}.'
+             .format(token2, alias2))
     c, d = alias_token(pushbots=pushbots, token=token2,
                        alias=alias2, platform=platform)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
-    debug('\nTesting alias_alias() with current_alias:{0}, alias:{1}.'
-          .format(alias1, alias3))
+    d_msg = ('\nTesting alias_alias() with current_alias:{0}, alias:{1}.'
+             .format(alias1, alias3))
     c, d = alias_alias(pushbots=pushbots, current_alias=alias1,
                        alias=alias3, platform=platform)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token3, 'alias': alias3}
-    debug('\nTesting alias_data() with custom data:{0}.'.format(data))
+    d_msg = '\nTesting alias_data() with custom data:{0}.'.format(data)
     c, d = alias_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_tag(pushbots):
-    debug('\nTesting tag_token() with token:{0} and tag:{1}.'
-          .format(token1, tag1))
+    d_msg = ('\nTesting tag_token() with token:{0} and tag:{1}.'
+             .format(token1, tag1))
     c, d = tag_token(pushbots=pushbots, platform=platform,
                      token=token1, tag=tag1)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
-    debug('\nTesting tag_alias() with alias:{0} and tag:{1}.'
-          .format(alias2, tag2))
+    d_msg = ('\nTesting tag_alias() with alias:{0} and tag:{1}.'
+             .format(alias2, tag2))
     c, d = tag_alias(pushbots=pushbots, platform=platform,
                      alias=alias2, tag=tag2)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token5, 'tag': tag1}
-    debug('\nTesting tag_data() with data:{0}'.format(data))
+    d_msg = '\nTesting tag_data() with data:{0}'.format(data)
     c, d = tag_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_untag(pushbots):
-    debug('\nTesting untag_token() with token:{0} and tag:{1}.'
-          .format(token1, tag1))
+    d_msg = ('\nTesting untag_token() with token:{0} and tag:{1}.'
+             .format(token1, tag1))
     c, d = untag_token(pushbots=pushbots, platform=platform,
                        token=token1, tag=tag1)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
-    debug('\nTesting untag_alias() with alias:{0} and tag:{1}.'
-          .format(alias2, tag2))
+    d_msg = ('\nTesting untag_alias() with alias:{0} and tag:{1}.'
+             .format(alias2, tag2))
     c, d = untag_alias(pushbots=pushbots, platform=platform,
                        alias=alias2, tag=tag2)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token5, 'tag': tag1}
-    debug('\nTesting untag_data() with data:{0}.'.format(data))
+    d_msg = '\nTesting untag_data() with data:{0}.'.format(data)
     c, d = untag_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_device_location(pushbots):
-    debug('\nTesting device_location_token() with token:{0}.'.format(token2))
+    d_msg = '\nTesting device_location_token() with token:{0}.'.format(token2)
     c, d = device_location_token(pushbots=pushbots, platform=platform,
                                  token=token2, lat='82.241', lng='14.241')
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token3,
             'lat': 85.243, 'lng': 17.123}
-    debug('\nTesting device_location_data() with data:{0}.'.format(data))
+    d_msg = '\nTesting device_location_data() with data:{0}.'.format(data)
     c, d = device_location_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_badge(pushbots):
-    debug('\nTesting badge_token() with token:{0}.'.format(token2))
+    d_msg = '\nTesting badge_token() with token:{0}.'.format(token2)
     c, d = badge_token(pushbots=pushbots, platform=platform,
                        token=token2, setbadgecount=15)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token3,
             'setbadgecount': 16}
-    debug('\nTesting badge_data() with data:{0}.'.format(data))
+    d_msg = '\nTesting badge_data() with data:{0}.'.format(data)
     c, d = badge_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_push(pushbots):
-    debug('\nTesting push_token() with token:{0}'.format(token1))
+    d_msg = '\nTesting push_token() with token:{0}'.format(token1)
     c, d = push_token(pushbots=pushbots, platform=platform, token=token1,
                       msg='Test message', sound='mysound', badge='16',
                       payload={'mycustomfield': 'Custom field'})
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token2, 'msg': 'Test message 2',
             'badge': '16', 'payload': {'mycustomfield2': 'Custom field 2'}}
-    debug('\nTesting push_data() with data:{0}.'.format(data))
+    d_msg = '\nTesting push_data() with data:{0}.'.format(data)
     c, d = push_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_push_batch(pushbots):
-    debug('\nTesting push_batch() with tags:{0},except_tags:{1}'
-          .format([tag1], [tag2]))
+    d_msg = ('\nTesting push_batch() with tags:{0},except_tags:{1}'
+             .format([tag1], [tag2]))
     c, d = push_batch_tags(pushbots=pushbots, platform=platform,
                            msg='Test message 2', sound='mysound',
                            badge='16', schedule='2015-04-02T11:33:00',
                            tags=[tag1], except_tags=[tag2],
                            payload={'mycustomfield3': 'My custom field 3'})
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
-    debug('\nTesting push_batch() with alias:{0}'.format(alias2))
+    d_msg = '\nTesting push_batch() with alias:{0}'.format(alias2)
     c, d = push_batch_alias1(pushbots=pushbots, platform=platform,
                              msg='Test message 2', sound='mysound', badge='16',
                              schedule='2015-04-02T11:33:00', alias=alias2,
                              payload={'mycustomfield3': 'My custom field 3'})
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
-    debug('\nTesting push_batch() with except_alias:{0}'.format(alias3))
+    d_msg = '\nTesting push_batch() with except_alias:{0}'.format(alias3)
     c, d = push_batch_alias2(pushbots=pushbots, platform=platform,
                              msg='Test message 2', sound='mysound', badge='16',
                              schedule='2015-04-02T11:33:00',
                              except_alias=alias3,
                              payload={'mycustomfield3': 'My custom field 3'})
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'msg': 'Test message 3', 'sound': 'mysound',
             'tags': [tag1, tag2]}
-    debug('\nTesting push_batch() with data:{0}'.format(data))
+    d_msg = '\nTesting push_batch() with data:{0}'.format(data)
     c, d = push_batch_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_analytics(pushbots):
-    debug('\nTesting get_analytics()')
+    d_msg = '\nTesting get_analytics()'
     c, d = get_analytics(pushbots=pushbots)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
-    debug('\nTesting record_analytics_platform')
+    d_msg = '\nTesting record_analytics_platform'
     c, d = record_analytics_platform(pushbots=pushbots, platform=platform)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform}
-    debug('\nTesting record_analytics_data with data:{0}.'.format(data))
+    d_msg = '\nTesting record_analytics_data with data:{0}.'.format(data)
     c, d = record_analytics_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
 
 def test_unregister(pushbots):
-    debug('\nTesting unregister_token() with token:{0}.'.format(token1))
+    d_msg = '\nTesting unregister_token() with token:{0}.'.format(token1)
     c, d = unregister_token(pushbots=pushbots, token=token1, platform=platform)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     data = {'platform': platform, 'token': token2}
-    debug('\nTesting unregister_data() with token:{0}.'.format(token2))
+    d_msg = '\nTesting unregister_data() with token:{0}.'.format(token2)
     c, d = unregister_data(pushbots=pushbots, data=data)
-    process_request(c, d)
+    process_request(d_msg, c, d)
 
     # Unregister rest tokens
     debug('\nUnregistering the rest of devices registered by tests.')
